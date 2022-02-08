afbebf58e83d19ec48033cc8b39552b6f2801ab8
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2022-02-08 00:16:25 -0300

    updade sobre mim

--- resources/views/eu.blade.php
+++ resources/views/eu.blade.php
@@ -16,6 +16,25 @@
         body {
             background-color: #ebe8e8;
         }
+        .text-eu {
+            padding: 5%;
+            text-align: justify;
+
+        }
+        .text-p-1 {
+            font-weight: 800;
+        }
+        .card-title {
+            font-size: 1.2rem;
+            margin: 2% 0;
+            font-family: 'Ubuntu';
+            font-weight: 600;
+
+        }
+        .card-name{
+            font-family: 'Ubuntu', sans-serif;
+        }
+
 
     </style>
 
@@ -23,56 +42,44 @@
     </div>
 
     <div class="container-fluid text-center">
-        <div class="row">
-            <div class="card">
-                <h1 class="card-title">Gabriel Fernandes</h1>
-                <p class="card-description"> Fotógrafo Profissional, Desenvolvedor FullStack && investidor/dayTrade</p>
-            </div>
-        </div>
     </div>
     <div class="container-fluid mt-5">
         <div class="img-fluid">
             <div class="container-fluid">
                 <div class="container-fluid eu">
-                    <img src="/image/_RSF7724.jpg" class="img-fluid banner-eu" alt="">
+                    <a href="/image/_RSF7724.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox">
+                        <img src="/image/_RSF7724.jpg" class="img-fluid banner-eu" alt="">
+                    </a>
                 </div>
             </div>
-            <h3 class="text-center mt-3">Um pouco sobre mim</h3>
-            <p class="description text-success container-fluid mt-3 flex-wap">
+            <div class="text-eu">
+                <h1 class="text-center mb-3 card-name">Gabriel Fernandes</h1>
+                <p class="container text-center text-p-1">
+                    Atualmente sou desenvolvedor fullstack/DevOps jr - Laravel
+                </p>
+                <p class="description text-success container-fluid mt-3 flex-wap">
+                    😉 Oi, me chamo Gabriel, tenho 34 anos, há dois anos comecei a estudar programação. Antes trabalhava como
+                    Fotógrafo profissional. Entrei no mundo da programação por curiosidade. Resolvir seguir na área de
+                    desenvolvimento web. Me dediquei 100% à programação, comecei com HTML e CSS pela Rockeseat. Tenho mais de
+                    237 aulas só nessa plataforma, onde foi minha base na programação.
 
-                😉 Oi, me chamo Gabriel, tenho 34 anos, há dois anos comecei a estudar programação. Antes trabalhava como
-                Fotógrafo profissional. Entrei no mundo da programação por curiosidade. Me interessei muito pela área de
-                desenvolvimento web. Me dediquei 100% à programação, comecei com HTML e CSS pela Rockeseat. Tenho mais de
-                237 aulas só nessa plataforma, onde foi minha base na programação.
 
-                https://app.rocketseat.com.br/me/gabrielfernandesweb
+                    ツ Atualmente estudo Laravel, tenho mais de 11 certificados, estou me especializando nesse framework full
+                    stack
 
-                ツ Atualmente estudo Laravel, tenho mais de 11 certificados, estou me especializando nesse framework full
-                stack
+                    Atualmente consigo desenvolver uma aplicação do zero, e colocar no ar, atuando também como DevOps 😎
 
-                Atualmente consigo desenvolver uma aplicação do zero, e colocar no ar, atuando também como DevOps 😎
+                    Skills: CSS3, HTML5. Javascript, MySQL, Linux, Docker, Bootstrap, Tailwind, Laravel, Stripe, Git e Github,
+                    Git Workflow,
 
-                Skills: CSS3, HTML5. Javascript, MySQL, Linux, Docker, Bootstrap, Tailwind, Laravel, Stripe, Git e Github,
-                Git Workflow,
+                </p>
+                <div class="container-fluid bg-grid">
+                    <p class="text-center">
+                        Buscando uma oportunidade como desenvolvedor.
+                    </p>
+                </div>
 
-                Coloquei 3 sites no ar, um landing page, um site pro meu aniversario, e outro pra turma de patins aqui da
-                minha cidade.
-                meuevento.tech / gabrielfernandesweb.online / turmadopatins.tech
-
-                A landing page era onde eu falava um pouco sobre minhas habilidades e postava meu currículo.
-
-                O site do meu aniversário eu fiz algo com banco de dado relacionais. No site falava sobre minha história de
-                vida e tinha uma parte pra quem tivesse acesso a um QRcode se registrar no site, postar uma foto e deixar um
-                comentário. Quem tivesse acesso também poderia curtir a foto de outras pessoas. foi um site mais pessoal
-                mesmo. Todo mundo podia ver, mas, interagir apenas com QRcode pra conseguir se registrar no site.
-
-                O site turma do patins eu fiz pensando em promover os passeios de patins. quando tivesse algum passeio,
-                aniversário, as pessoas poderia postar no site sobre o evento e quem tivesse interesse em ir, era só clicar
-                no evento participar;
-
-                Atualmente os meus sites estão fora do ar, ainda estou em busca do primeiro emprego na área de TI. Não tive
-                condições financeiras de deixar eles rodando.
-            </p>
+            </div>
 
         </div>
     </div>
